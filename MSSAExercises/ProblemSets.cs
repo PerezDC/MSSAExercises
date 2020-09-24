17a4d097f9603c9d81ec67bb4a549a1ee4e38814
PerezDC <61435556+PerezDC@users.noreply.github.com>
2020-09-23 22:07:59 -0700

    Add project files.

---
+++ MSSAExercises/ProblemSets.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathGames
+{
+    class ProblemSets
+    {
+        private int _score;
+
+        public static int Score { get; set; }
+
+        static Random r = new Random();
+
+        struct Flashcards
+        {
+            public int num1;
+            public int num2;
+        }
+
+        static Flashcards[] GetQuestions(int size, bool division = false)
+        {
+            Flashcards[] questionSet = new Flashcards[size];
+
+            if (division == true)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    questionSet[i] = new Flashcards
+                    {
+                        num1 = r.Next(1,13),
+                        num2 = r.Next(1,13)
+                    };
+
+                }
+            }
+            else
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    questionSet[i] = new Flashcards
+                    {
+                        num1 = r.Next(13),
+                        num2 = r.Next(13)
+                    };
+
+                }
+            }
+
+            return questionSet;
+        }
+
+        public static void AdditionQuestions(int numQuestions)
+        {
+            Flashcards[] additionSet = GetQuestions(numQuestions);
+
+            for (int i = 0; i < additionSet.Length; i++)
+            {
+                Console.Write($"\nQuestion {i + 1}. \n{additionSet[i].num1} + {additionSet[i].num2} = ");
+                int userAnswer = Convert.ToInt32(Console.ReadLine());
+                if (userAnswer == additionSet[i].num1 + additionSet[i].num2)
+                {
+                    Console.WriteLine("Correct.");
+                    Score++;
+                }
+                else
+                {
+                    Console.WriteLine($"Incorrect. Correct answer is {additionSet[i].num1 + additionSet[i].num2}.");
+                }
+            }
+        }
+
+        public static void MultiplicationQuestions(int numQuestions)
+        {
+            Flashcards[] multiSet = GetQuestions(numQuestions);
+
+            for (int i = 0; i < multiSet.Length; i++)
+            {
+                Console.Write($"\nQuestion {i + 1}. \n{multiSet[i].num1} * {multiSet[i].num2} = ");
+                int userAnswer = Convert.ToInt32(Console.ReadLine());
+                if (userAnswer == multiSet[i].num1 * multiSet[i].num2)
+                {
+                    Console.WriteLine("Correct.");
+                    Score++;
+                }
+                else
+                {
+                    Console.WriteLine($"Incorrect. Correct answer is {multiSet[i].num1 * multiSet[i].num2}.");
+                }
+            }
+        }
+
+        public static void SubtractionQuestions(int numQuestions)
+        {
+            Flashcards[] SubtractSet = GetQuestions(numQuestions);
+
+            for (int i = 0; i < SubtractSet.Length; i++)
+            {
+                if (SubtractSet[i].num1 < SubtractSet[i].num2) // Swap numbers if the left number is less than the left.
+                {
+                    SubtractSet[i].num1 += SubtractSet[i].num2;
+                    SubtractSet[i].num2 = SubtractSet[i].num1 - SubtractSet[i].num2;
+                    SubtractSet[i].num1 -= SubtractSet[i].num2;
+                }
+
+                Console.Write($"\nQuestion {i + 1}. \n{SubtractSet[i].num1} - {SubtractSet[i].num2} = ");
+                int userAnswer = Convert.ToInt32(Console.ReadLine());
+                if (userAnswer == SubtractSet[i].num1 - SubtractSet[i].num2)
+                {
+                    Console.WriteLine("Correct.");
+                    Score++;
+                }
+                else
+                {
+                    Console.WriteLine($"Incorrect. Correct answer is {SubtractSet[i].num1 - SubtractSet[i].num2}.");
+                }
+            }
+        }
+
+        public static void DivisionQuestions(int numQuestions)
+        {
+            Flashcards[] DivisionSet = GetQuestions(numQuestions, true);
+
+            for (int i = 0; i < DivisionSet.Length; i++)
+            {
+                Console.Write($"\nQuestion {i + 1}. \n{DivisionSet[i].num1} / {DivisionSet[i].num2} = ");
+                double userAnswer = Convert.ToDouble(Console.ReadLine());
+                if (userAnswer == Convert.ToDouble(DivisionSet[i].num1 / DivisionSet[i].num2))
+                {
+                    Console.WriteLine("Correct.");
+                    Score++;
+                }
+                else
+                {
+                    Console.WriteLine($"Incorrect. Correct answer is {DivisionSet[i].num1 / DivisionSet[i].num2}.");
+                }
+            }
+        }
+    }
+}
---
+++ MSSAExercises/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace MathGames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programming Exercise 12 | Math Games");
+            Console.WriteLine("EX 3B - 3C - DPerez - 09/23/2020");
+            /*
+             * four "sets" of flash cards: addition, subtraction, multiplication, and division
+             * beginning with, ask user the type of problem they want to practice and number of problems.
+             * echo problem type and number of problems to the user
+             */
+            Console.WriteLine("\nWelcome to Math Games!");
+            Console.WriteLine("Which type of problem would you like to practice?");
+            Console.WriteLine("\tAddition, enter 1");
+            Console.WriteLine("\tSubtraction, enter 2");
+            Console.WriteLine("\tMultiplication, enter 3");
+            Console.WriteLine("\tDivision, enter 4");
+            Console.Write("\nEnter your selection: ");
+
+            int problemType = Convert.ToInt32(Console.ReadLine());
+            while (problemType < 1 || problemType > 4)
+            {
+                Console.Write("\nInvalid input! Enter a number ranging from 1 to 4: ");
+                problemType = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("\nHow many questions would you like to receive (max of 12): ");
+            int practiceAmount = Convert.ToInt32(Console.ReadLine());
+            while (practiceAmount < 1 || practiceAmount > 12)
+            {
+                Console.Write("\nInvalid input! Enter a number ranging from 1 to 12: ");
+                practiceAmount = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if (problemType == 1)
+            {
+                Console.WriteLine($"\nProblem type: Addition | Number of questions: {practiceAmount}.");
+                ProblemSets.AdditionQuestions(practiceAmount);
+            }
+            else if (problemType == 2)
+            {
+                Console.WriteLine($"\nProblem type: Subtraction | Number of questions: {practiceAmount}.");
+                ProblemSets.SubtractionQuestions(practiceAmount);
+            }
+            else if (problemType == 3)
+            {
+                Console.WriteLine($"\nProblem type: Multiplication | Number of questions: {practiceAmount}.");
+                ProblemSets.MultiplicationQuestions(practiceAmount);
+            }
+            else if (problemType == 4)
+            {
+                Console.WriteLine($"\nProblem type: Division | Number of questions: {practiceAmount}.");
+                ProblemSets.DivisionQuestions(practiceAmount);
+            }
+
+            Console.WriteLine($"Your score was {ProblemSets.Score} / {practiceAmount}. You got");
+        }
+    }
+}
