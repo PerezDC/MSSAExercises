d0d780bb90095f23712b6ec989b175168e86ab17
PerezDC <61435556+PerezDC@users.noreply.github.com>
2020-09-25 23:58:09 -0700

    Added Password Encryption Ex

---
+++ PWEncryptAuth/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PWEncryptAuth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EX 3A - C# - Password Encryption and Authentication");
+            /*
+             * Interface should have three options:
+                  - save a new password for a specific username,
+                  - authenticate a specific username/password pair, or
+                  - exit the application.
+             */
+            Console.WriteLine("--------------------------------------------------------------------");
+            Console.WriteLine("\n\tPASSWORD AUTHENTICATION SYSTEM");
+            Console.WriteLine("\n\tPlease select one option: .");
+            Console.WriteLine("\t1. Establish an account ");
+            Console.WriteLine("\t2. Authenticate a user ");
+            Console.WriteLine("\t3. Exit the system");
+            Console.Write("\n\tEnter selection: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("\n--------------------------------------------------------------------");
+            if (userInput == "1")
+            {
+                UserDetails.CreateNew();
+
+                foreach (var item in UserDetails.usersPasswords)
+                {
+                    Console.WriteLine(item.Key + " " + item.Value);
+                }
+            }
+
+
+        }
+    }
+
+}
---
+++ PWEncryptAuth/UserDetails.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+using System.Text;
+
+namespace PWEncryptAuth
+{
+    class UserDetails
+    {
+        public static Dictionary<string, string> usersPasswords = new Dictionary<string, string>();
+
+        public static void CreateNew()
+        {
+            string userName = NewUsername();
+            string userPassword = NewPassword();
+
+            usersPasswords.Add(userName, userPassword);
+            Console.WriteLine($"\n\n\tThanks {userName}! Username and password saved!");
+        }
+
+        static string NewUsername()
+        {
+            Console.Write("\n\tEnter a username: ");
+            string userId = Console.ReadLine();
+            if (usersPasswords.ContainsKey(userId))
+            {
+                Console.Write("\tUsername taken! Try another: ");
+                userId = Console.ReadLine();
+            }
+
+            Console.WriteLine($"\t{userId} is available.");
+            return userId;
+        }
+
+        static string NewPassword()
+        {
+            string unEncryptPwd, encryptedPwd;
+
+            Console.Write("\tEnter a password: ");
+            unEncryptPwd = Console.ReadLine();
+            //TODO: add encryption body
+
+            return encryptedPwd;
+        }
+
+
+    }
+
+}
