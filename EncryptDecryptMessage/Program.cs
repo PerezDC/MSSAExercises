16b090c49712c1f9196f2b6e46f2287ae48f19e9
PerezDC <61435556+PerezDC@users.noreply.github.com>
2020-09-30 21:52:47 -0700

    Added support for generating a multikey. Updated the CodePlainText method to support all keys vice single key.

--- EncryptDecryptMessage/Program.cs
+++ EncryptDecryptMessage/Program.cs
@@ -18,27 +18,58 @@ namespace EncryptDecryptMessage
             return charValue;
         }
 
-        static string CodePlainText(string plainText, int singleKey)
+        static (string, string) CodePlainText(string plainText, string key)
         {
             string codedMessage = "";
+            string cleanPlainText = "";
+            int keyCounter = 0;
 
-            for (int i = 0; i < plainText.Length; i++)
+                for (int i = 0; i < plainText.Length; i++)
+                {
+                    if (keyCounter == key.Length)
+                    {
+                        keyCounter = 0;
+                    }
+
+                    int singleKey = KeyIntValue(key[keyCounter]);
+
+                    if (plainText[i] < 65 || plainText[i] < 97 && plainText[i] > 90 || plainText[i] > 122)
+                    {
+                        continue;
+                    }
+                    if (plainText[i] <= 90 && plainText[i] + singleKey > 90 ||
+                             plainText[i] >= 97 && plainText[i] + singleKey > 122)
+                    {
+                        codedMessage += (char) (plainText[i] + singleKey - 26);
+                        cleanPlainText += (char) (plainText[i] + singleKey);
+                    }
+                    else
+                    {
+                        codedMessage += (char) (plainText[i] + singleKey);
+                        cleanPlainText += (char) (plainText[i] + singleKey);
+                    }
+
+                    keyCounter++;
+                }
+
+            return (codedMessage, cleanPlainText);
+        }
+
+        static string GetMultiKey(string multiKeyPlain, string cleanPlainText)
+        {
+            string multiKeyCoded = "";
+            int counter = 0;
+
+            for (int i = 0; counter < cleanPlainText.Length; counter++)
             {
-                if (plainText[i] < 65 || plainText[i] < 97 && plainText[i] > 90 || plainText[i] > 122)
+                if (i == multiKeyPlain.Length)
                 {
-                    continue;
+                    i = 0;
                 }
-                else if (plainText[i] <= 90 && plainText[i] + singleKey > 90 || plainText[i] >= 97 && plainText[i] + singleKey > 122) 
-                {
-                    codedMessage += Convert.ToChar(plainText[i] + singleKey - 26);
-                }
-                else
-                {
-                    codedMessage += Convert.ToChar(plainText[i] + singleKey);
-                }
+                multiKeyCoded += multiKeyPlain[i];
+                i++;
             }
-
-            return codedMessage;
+            return multiKeyCoded;
         }
 
         static void Main(string[] args)
@@ -51,11 +82,22 @@ namespace EncryptDecryptMessage
             string plainText = Console.ReadLine();
 
             Console.Write("\nEnter your single key as an alpha character: ");
-            int singleKey = KeyIntValue(Convert.ToChar(Console.ReadLine()));
+            string singleKey = Console.ReadLine();
 
-            string codedMessage = CodePlainText(plainText, singleKey);
-            Console.WriteLine($"Encrypted message with single key is [{codedMessage}]");
+            Console.Write("\nEnter your multi key as alpha characters: ");
+            string plainMultiKey = Console.ReadLine();
 
+            Console.WriteLine($"\nYou entered [{plainText}] as plain text");
+            Console.WriteLine($"You entered [{singleKey}] as you single key");
+            Console.WriteLine($"You entered [{plainMultiKey}] as your multi key");
+
+            var (codedMessageSingle, cleanPlainText) = CodePlainText(plainText, singleKey);
+            string multiKey = GetMultiKey(plainMultiKey, cleanPlainText);
+            string codedMessageMulti = CodePlainText(plainText, multiKey).Item1;
+
+            Console.WriteLine($"\nEncrypted message with single key is [{codedMessageSingle}]");
+            Console.WriteLine($"Encrypted message with multi key is [{codedMessageMulti}]");
+            Console.WriteLine("Encrypted message with continuous key is [{}]");
         }
     }
 }
