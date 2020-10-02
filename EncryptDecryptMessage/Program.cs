fb1492f6f099f4bf384869efb00b7a2db1045937
PerezDC <61435556+PerezDC@users.noreply.github.com>
2020-10-01 21:19:28 -0700

    Added decryption body to program.

--- EncryptDecryptMessage/Program.cs
+++ EncryptDecryptMessage/Program.cs
@@ -2,28 +2,30 @@
 
 namespace EncryptDecryptMessage
 {
-    class Program
+    public class Program
     {
-        static int KeyIntValue(char singleKey)
+        public static int KeyIntValue(char singleKey)
         {
-            int charValue = 0;
-            if ((int)singleKey >= 97)
+            int charValue;
+            if (singleKey >= 97)
             {
-                charValue = (int)singleKey - 96;
+                charValue = singleKey - 96;
             }
             else
             {
-                charValue = (int)singleKey - 64;
+                charValue = singleKey - 64;
             }
             return charValue;
         }
 
-        static (string, string) CodePlainText(string plainText, string key)
+        public static (string, string) EncryptDecryptText(string plainText, string key, bool decrypt = false)
         {
             string codedMessage = "";
             string cleanPlainText = "";
             int keyCounter = 0;
 
+            if (decrypt == false)
+            {
                 for (int i = 0; i < plainText.Length; i++)
                 {
                     if (keyCounter == key.Length)
@@ -38,41 +40,75 @@ namespace EncryptDecryptMessage
                         continue;
                     }
                     if (plainText[i] <= 90 && plainText[i] + singleKey > 90 ||
-                             plainText[i] >= 97 && plainText[i] + singleKey > 122)
+                        plainText[i] >= 97 && plainText[i] + singleKey > 122)
                     {
                         codedMessage += (char) (plainText[i] + singleKey - 26);
-                        cleanPlainText += (char) (plainText[i] + singleKey);
+                        cleanPlainText += plainText[i];
                     }
                     else
                     {
                         codedMessage += (char) (plainText[i] + singleKey);
-                        cleanPlainText += (char) (plainText[i] + singleKey);
+                        cleanPlainText += plainText[i];
                     }
-
                     keyCounter++;
                 }
+            }
+            else
+            {
+                for (int i = 0; i < plainText.Length; i++)
+                {
+                    if (keyCounter == key.Length)
+                    {
+                        keyCounter = 0;
+                    }
 
+                    int singleKey = KeyIntValue(key[keyCounter]);
+
+                    if (plainText[i] <= 90 && plainText[i] - singleKey < 65 || plainText[i] >= 97 && plainText[i] - singleKey < 97)
+                    {
+                        codedMessage += (char)(plainText[i] - singleKey + 26);
+                    }
+                    else
+                    {
+                        codedMessage += (char)(plainText[i] - singleKey);
+                    }
+                    keyCounter++;
+                }
+            }
             return (codedMessage, cleanPlainText);
         }
 
-        static string GetMultiKey(string multiKeyPlain, string cleanPlainText)
+        public static string GetMultiKey(string plainMultiKey, string cleanPlainText)
         {
             string multiKeyCoded = "";
             int counter = 0;
 
             for (int i = 0; counter < cleanPlainText.Length; counter++)
             {
-                if (i == multiKeyPlain.Length)
+                if (i == plainMultiKey.Length)
                 {
                     i = 0;
                 }
-                multiKeyCoded += multiKeyPlain[i];
+                multiKeyCoded += plainMultiKey[i];
                 i++;
             }
             return multiKeyCoded;
         }
 
-        static void Main(string[] args)
+        public static string GetContinuousKey(string plainMultiKey, string cleanPlainText)
+        {
+            string tempKey = plainMultiKey;
+            int keySize = cleanPlainText.Length;
+
+            for (int i = 0; tempKey.Length < keySize; i++)
+            {
+                tempKey += cleanPlainText[i];
+            }
+
+            return tempKey;
+        }
+
+        public static void Main(string[] args)
         {
             Console.WriteLine("Programming Exercise 14");
             Console.WriteLine("Encrypting and Decrypting Messages");
@@ -81,23 +117,32 @@ namespace EncryptDecryptMessage
             Console.Write("\nEnter plain text: ");
             string plainText = Console.ReadLine();
 
-            Console.Write("\nEnter your single key as an alpha character: ");
+            Console.Write("Enter your single key as an alpha character: ");
             string singleKey = Console.ReadLine();
 
-            Console.Write("\nEnter your multi key as alpha characters: ");
+            Console.Write("Enter your multi key as alpha characters: ");
             string plainMultiKey = Console.ReadLine();
 
             Console.WriteLine($"\nYou entered [{plainText}] as plain text");
-            Console.WriteLine($"You entered [{singleKey}] as you single key");
+            Console.WriteLine($"You entered [{singleKey}] as your single key");
             Console.WriteLine($"You entered [{plainMultiKey}] as your multi key");
 
-            var (codedMessageSingle, cleanPlainText) = CodePlainText(plainText, singleKey);
+            var (codedMessageSin, cleanPlainText) = EncryptDecryptText(plainText, singleKey);
             string multiKey = GetMultiKey(plainMultiKey, cleanPlainText);
-            string codedMessageMulti = CodePlainText(plainText, multiKey).Item1;
+            string codedMessageMult = EncryptDecryptText(plainText, multiKey).Item1;
+            string continuousKey = GetContinuousKey(plainMultiKey, cleanPlainText);
+            string codedMessageCont = EncryptDecryptText(plainText, continuousKey).Item1;
 
-            Console.WriteLine($"\nEncrypted message with single key is [{codedMessageSingle}]");
-            Console.WriteLine($"Encrypted message with multi key is [{codedMessageMulti}]");
-            Console.WriteLine("Encrypted message with continuous key is [{}]");
+            Console.WriteLine($"\nEncrypted message with single key is [{codedMessageSin}]");
+            Console.WriteLine($"Encrypted message with multi key is [{codedMessageMult}]");
+            Console.WriteLine($"Encrypted message with continuous key is [{codedMessageCont}]");
+
+            Console.WriteLine($"\nDecrypted message with single key is [{EncryptDecryptText(codedMessageSin, singleKey, true).Item1}]");
+            Console.WriteLine($"Decrypted message with multi key is [{EncryptDecryptText(codedMessageMult,multiKey, true).Item1}]");
+            Console.WriteLine($"Decrypted message with continuous key is [{EncryptDecryptText(codedMessageCont,continuousKey, true).Item1}]");
+
+            Console.Write("\nPress any key to continue . . .");
+            Console.ReadKey();
         }
     }
 }
